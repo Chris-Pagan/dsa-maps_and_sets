91d6c32b4c400229efa187ee95e170133bf8f692
Chris-Pagan <christopher.pagan@outlook.com>
2022-01-04 05:39:57 -0800

    all qualified tests pass

--- src/equalArrays.js
+++ src/equalArrays.js
@@ -1,6 +1,11 @@
 /**
  * return true if two arrays are equal, false otherwise
  */
-function isEqual(a1, a2) {}
+ function isEqual(a1, a2) {
+    a1.sort()
+    a2.sort()
+    return Array.isArray(a1) && Array.isArray(a2) && a1.length === a2.length && a1.every((value, index) => value === a2[index])
+
+}
 
 module.exports = isEqual;
--- src/setOperations.js
+++ src/setOperations.js
@@ -1,16 +1,29 @@
 /**
  * return the union of two sets
  */
-function union(s1, s2) {}
+function union(s1, s2) {
+    if (s1.size === 0 && s2.size === 0){
+        return new Set();
+    } else if (s2.size === 0){
+        return s1;
+    } else if (s1.size === 0){
+        return s2
+    }
+    return new Set([...s1, ...s2]);
+}
 
 /**
  * return the intersection of two sets
  */
-function intersect(s1, s2) {}
+function intersect(s1, s2) {
+    return new Set([...s1].filter((i) => s2.has(i)));
+}
 
 /**
  * return the difference of two sets
  */
-function difference(s1, s2) {}
+function difference(s1, s2) {
+    return new Set([...s1].filter((i) => !s2.has(i)));
+}
 
 module.exports = { union, intersect, difference };
